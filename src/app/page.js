9c0f715b91314f212a51f9b7a82b0e5fe5f517c6
SultanIndian007 <amantiwari201608@gmail.com>
2023-11-16 20:42:35 +0530

    final changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Screen from '@/components/calci/Screen'
 import Keyboard from '@/components/calci/Keyboard'
 
 const calculator = () => {
   return (
     <div>
-      <Screen/>
       <Keyboard/>
     </div>
   )
